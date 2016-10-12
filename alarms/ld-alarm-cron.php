e84ef46a655c17ad124fb44e02cc08120d6b4c9f
Tyler Johnson <tyler@connxus.com>
2016-10-12 14:15:46 -0400

    Minro update.

--- alarms/ld-alarm-cron.php
+++ alarms/ld-alarm-cron.php
@@ -17,15 +17,39 @@ $shellPath = "/var/www/html/linux-dash/server/modules/shell_files";
 
 if (isset($argv[1]) && isset($argv[2])) {
 	$behavior = trim(strtolower($argv[1]));
+	$serverName = trim(strtolower($argv[1]));
 
 	// Report status
 	if ('report' == $behavior) {
-		$messageText = 'Test.';
 		$diskRaw = shell_exec("{$shellPath}/disk_partitions.sh");
-		echo "\n{$diskRaw}\n\n";
 		$diskJSON = json_decode($diskRaw);
-		// exec("curl -X POST --data-urlencode 'payload={\"text\": \"{$messageText}\"}' {$slackWebHookUrl}");
+	
+		$messageText = "[{$serverName}] Disk Usage Status";
+		$attachments = array();	
 		var_dump($diskJSON);
+		foreach ($diskJSON as $mount) {
+			$obj = new stdClass();
+			$obj->fallback = "Disk Usage Status Report for {$serverName}";
+			$obj->pretext = "Disk Usage Status Report for {$serverName}";
+			$obj->color = "#46569f";
+			$obj->title = $mount['file_system'] . '[' . $mount['mounted'] . ']';
+			$obj->fields = array();
+
+			$obj->fields[0] = new stdClass();
+			$obj->fields[0]->title = 'Used/Avail';
+			$obj->fields[0]->value = $mount['used'] . ' / ' . $mount['avail'];
+			$obj->fields[0]->short = true;
+
+			$obj->fields[0] = new stdClass();
+			$obj->fields[0]->title = 'Used';
+			$obj->fields[0]->value = $mount['used%'];
+			$obj->fields[0]->short = true;
+
+
+			$attachments[] = $obj;
+		}
+		$attachmentTxt = json_encode($attachments);
+		exec("curl -X POST --data-urlencode 'payload={\"text\": \"{$messageText}\", \"attachments\": {$attachmentTxt}}' {$slackWebHookUrl}");
 	} 
 	// Check all alarms	
 	elseif ('monitor' == $behavior) {
