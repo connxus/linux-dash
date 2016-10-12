eda8f56707cd92906fe2ed5082427691467de2c6
Tyler Johnson <tyler@connxus.com>
2016-10-12 13:02:01 -0400

    Minor update.

--- alarms/ld-alarm-cron.php
+++ alarms/ld-alarm-cron.php
@@ -1,9 +1,41 @@
 <?php
 
-$slackWebHookUrl = "https://hooks.slack.com/services/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";
-$messageText = 'Test.';
+/*
+This script should be scheduled on cron to report server status.
 
-exec("curl -X POST --data-urlencode 'payload={\"text\": \"{$messageText}\"} {$slackWebHookUrl}");
+Usage:
+
+ld-alarm-cron.php {behavior} {serverName}
+
+The behavior argument accepts two values:
+"monitor" will send alerts to #server-status slack channel if criteria exceeds defined thresholds.
+"report" will send a status summary to #server-status slack channel.
+
+*/
+$slackWebHookUrl = "https://hooks.slack.com/services/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";
+$shellPath = "/var/www/html/linux-dash/server/modules/shell_files";
+
+if (isset($argv[1]) && isset($argv[2])) {
+	$behavior = trim(strtolower($argv[1]));
+
+	// Check all alarms
+	if ('monitor' == $behavior) {
+		$messageText = 'Test.';
+		$diskRaw = shell_exec("{$shellPath}/disk_partitions.sh");
+		echo "\n{$diskRaw}\n";
+		// exec("curl -X POST --data-urlencode 'payload={\"text\": \"{$messageText}\"}' {$slackWebHookUrl}");
+	}
+
+	// Report status
+	if ('monitor' == $behavior) {
+
+		$messageText = 'Test.';
+		// exec("curl -X POST --data-urlencode 'payload={\"text\": \"{$messageText}\"}' {$slackWebHookUrl}");
+	}
+	
+} else {
+	echo "Usage: ld-alarm-cron.php (monitor|report) serverName";
+}
 
 
 
